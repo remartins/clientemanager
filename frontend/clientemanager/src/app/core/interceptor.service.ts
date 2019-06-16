eff59e6516030648f8451cd79fe8784a0c1f4f8d
Renato <renatomartinsti@gmail.com>
2019-06-16 15:24:34 -0300

    ajuste no interceptor

--- frontend/clientemanager/src/app/core/interceptor.service.ts
+++ frontend/clientemanager/src/app/core/interceptor.service.ts
@@ -3,9 +3,9 @@ import { Router } from '@angular/router';
 import { ApiService } from './api.service';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { MessageService } from 'primeng/api';
 import { Observable, empty } from 'rxjs';
-
+import { MessageService } from './message.service';
+import { debug } from 'util';
 
 
 @Injectable({
@@ -14,7 +14,8 @@ import { Observable, empty } from 'rxjs';
 export class InterceptorService implements HttpInterceptor {
 
   constructor(private apiService: ApiService,
-    private router: Router) { }
+              private router: Router,
+              private messageService: MessageService) { }
 
   private isRequestLogin(request: HttpRequest<any>) {
     return request.url === "http://localhost:8080/oauth/token";
@@ -35,28 +36,29 @@ export class InterceptorService implements HttpInterceptor {
         return event;
       }),
       catchError((error => {
+        debugger;
         if (error instanceof HttpErrorResponse) {
           switch (error.status) {
             case 409:
-              //this.messageService.showWarning('Falha de registro', 'O e-mail utilizado no cadastro está sendo usado por outro usuário!');
+              this.messageService.showWarning('O e-mail utilizado no cadastro está sendo usado por outro usuário!');
               return this.handleErrorGeneral(error);
             case 404:
-              //this.messageService.showError('Usuário não encontrado', 'Favor verificar se o seu e-mail foi didigato corretamente');
+              this.messageService.showError('Favor verificar se o seu e-mail foi didigato corretamente');
               return this.handleErrorGeneral(error);
             case 403:
               console.log('error 403');
               return this.getAccessToken(request, next);
             case 0:
-              console.log('error 0');
-              localStorage.removeItem('accessToken');
-              return this.getAccessToken(request, next);
+             console.log('error 0');
+             localStorage.removeItem('accessToken');
+             return this.getAccessToken(request, next);
             case 401:
-              return this.handle401Error(error);
+             return this.handle401Error(error);
             case 400:
-              //this.messageService.showError('Falha de autenticação', 'Usuário ou senha invávalidos');
+              this.messageService.showError('Usuário ou senha invávalidos');
               return this.router.navigate(['login']);
             case 303:
-              return this.handle303Error(error);
+             return this.handle303Error(error);
           }
         }
         Observable.throw(error);
@@ -64,46 +66,39 @@ export class InterceptorService implements HttpInterceptor {
   }
   private getAccessToken(req: HttpRequest<any>, next: HttpHandler): Observable<any> {
     return this.apiService.getAccessToken(localStorage.getItem('refreshToken'))
-      .pipe(
-        switchMap(
-          resp => {
+    .pipe(
+    switchMap(
+        resp => {
             localStorage.setItem('accessToken', resp.access_token);
             const token = localStorage.getItem('accessToken');
             req = req.clone({ headers: req.headers.set('Authorization', 'Bearer ' + token) });
             return next.handle(req);
-          }
-        ));
+        }
+    ));
   }
 
   handleErrorGeneral(error) {
-    // if (error.status === 409 || error.status === 404) {
-    //   return empty();
-    // }
-    return this.router.navigate(['login']);
-    //return empty();
+    if ( error.status === 409 || error.status === 404 ) {
+      return empty();
+    }
+    return empty();
   }
 
   handle303Error(error) {
     if (error.error.message === 'invalidToken') {
-      //this.messageService.showError('Vericação de registro', 'Token Inválido, favor solicitar novo token');
-      //return this.router.navigate(['resend-register-token']);
-      return this.router.navigate(['login']);
+      this.messageService.showError('Token Inválido, favor solicitar novo token');
+      return this.router.navigate(['resend-register-token']);
     } else if (error.error.message === 'expired') {
-      //this.messageService.showError('Vericação de registro', 'Token expirou, favor solicitar novo token');
-      //return this.router.navigate(['resend-register-token']);
-      return this.router.navigate(['login']);
+      this.messageService.showError('Token expirou, favor solicitar novo token');
+      return this.router.navigate(['resend-register-token']);
     }
-    //return EmptyObservable.create();
-    //return empty();
-    return this.router.navigate(['login']);
+    return empty();
   }
   handle401Error(error) {
     if (error.error.error_description === 'UserNotEnabled') {
-      //this.messageService.showError('Usuário não está habilitado', 'Favor habilitar o seu acesso atgravés do e-mail de verificação');
+      this.messageService.showError('Favor habilitar o seu acesso atgravés do e-mail de verificação');
       return this.router.navigate(['login']);
     }
-    //return EmptyObservable.create();
-    //return this.router.navigate(['login']);
     return empty();
   }
 }
